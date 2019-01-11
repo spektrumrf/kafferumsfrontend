d6fe22d87950adb9501f823847d170e35c515b68
Walter Grönholm <waltergronholm@gmail.com>
2019-01-11 16:03:58 +0200

    Made HTTP calls Async, yet to fix ReactJS render...

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -1,25 +1,40 @@
 
-function httpGetAsync(theUrl, callback) {
+function httpRequestAsync(targetUrl, verb, data, success, handleError) {
     var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.onreadystatechange = function() { 
-    //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    //         callback(xmlHttp.responseText);
-    // }
-    xmlHttp.open('GET', theUrl, false); // true for asynchronous 
-    xmlHttp.send(null);
-    return xmlHttp.responseText //comment this for async
+	xmlHttp.onreadystatechange = () => {
+		if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+			success(xmlHttp.responseText, xmlHttp);
+		else if (handleError)
+			handleError(xmlHttp.responseText, xmlHttp);
+	};
+	xmlHttp.open(verb, targetUrl, true);
+	if (verb === 'POST') {
+		xmlHttp.setRequestHeader("Content-type", "application/json");
+		xmlHttp.send(JSON.stringify(data));
+	} else {
+		xmlHttp.send(null);
+	}
 }
 
-function httpPostAsync(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = () => { alert(xmlHttp.responseText); };
-	xmlHttp.open("POST", theUrl, true);
-	xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xmlHttp.send("pin=1234&userName=waxwax");
+function httpGetAsync(targetUrl, success, handleError) {
+	httpRequestAsync(targetUrl, 'GET', null, success, handleError)
+}
+
+function httpPostAsync(targetUrl, obj, success, handleError) {
+	httpRequestAsync(targetUrl, 'POST', obj, success, handleError)
 }
 
 function getUsernameList() {
-    return JSON.parse(httpGetAsync('http://localhost:25555/user/names', null));
+	var success = (data) => {
+		alert(JSON.parse(data));
+	}
+
+	//httpGetAsync('http://localhost:25555/api/user/names', success);
+	
+	var login = { userName: 'waxwax', pin: '1234' }
+	httpPostAsync('http://localhost:25555/api/user/pin', login, success, success);
+	
+	return [];
 }
 
 export default getUsernameList
